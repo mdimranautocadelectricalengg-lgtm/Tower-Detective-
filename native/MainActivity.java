c3db568be12b54311133a36ea2fc064c79c911f8
mdimranautocadelectricalengg-lgtm <mdimranautocadelectricalengg@gmail.com>
2026-08-25 11:47:21 +0530

    Add MainActivity for TowerDetective app

---
+++ native/MainActivity.java
@@ -0,0 +1,18 @@
+package com.towerdetective.app;
+
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        registerPlugin(
+            TowerDetectorPlugin.class
+        );
+
+        super.onCreate(savedInstanceState);
+    }
+}
